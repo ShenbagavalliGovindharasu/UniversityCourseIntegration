c8ed2ad1a7003e025288fd95a28915ee481cdc54
VigneshwariRajendran <51949214+VigneshwariRajendran@users.noreply.github.com>
2022-01-08 12:54:47 +0530

    Update UniversityCourseApplicationTests.java

--- UniversityCourse/src/test/java/com/univ/UniversityCourseApplicationTests.java
+++ UniversityCourse/src/test/java/com/univ/UniversityCourseApplicationTests.java
@@ -49,6 +49,9 @@ class UniversityCourseApplicationTests {
 	@MockBean
 	CourseRepository crepo;
 	
+	@MockBean
+	StaffRepository srepo;
+	
 	@Test
 	void contextLoads() {
 	}
@@ -120,6 +123,66 @@ class UniversityCourseApplicationTests {
 
 			assertThat(course).isEmpty();
 		}
+	
+	//adding staff details test
+	@Test
+	public void testinsertStaff() {
+		Staff s=new Staff();
+		
+		s.setId(155L);
+		s.setStaffname("Ram");
+		s.setCourse("CIVIL");
+		
+		when(srepo.save(s)).thenReturn(s);
+		assertThat(srepo.save(s)).isEqualTo(s);
+		
+		
+	}
+	
+	
+	
+	//view all staff
+	@Test
+	public void getAllStaff() {
+		List<Staff> staff=new ArrayList();
+		staff.add(new Staff());
+		given(srepo.findAll()).willReturn(staff);
+		List<Staff> expected = srepo.findAll();
+		assertEquals(expected, staff);
+		verify(srepo).findAll();
+		
+	}
+	
+
+	
+	//delete staff using id
+	@Test
+	public void deleteStaffbyId() {
+		Staff staff= new Staff();
+		staff.setId(114L);
+		staff.setStaffname("Sammu");
+		staff.setCourse("commerce");
+		when(srepo.findById(staff.getId())).thenReturn(Optional.of(staff));
+		srepo.deleteById(staff.getId());
+		verify(srepo).deleteById(staff.getId());
+	}
+	
+	//check if repo is empty
+	@Test
+	public void find_no_staff_if_repo_is_empty() {
+		Iterable<Staff> staff = srepo.findAll();
+
+		assertThat(staff).isEmpty();
+	}
+	
+	@Test
+    public void testDeleteStaff() {
+        Staff staff = new Staff(303L, "aishu", "MECH");
+        srepo.save(staff);
+        srepo.delete(staff);
+    }
+	
+
 	  
 
 }
