cfc65b4695777c2ad215d7425cbce74a7645e9e9
VigneshwariRajendran <51949214+VigneshwariRajendran@users.noreply.github.com>
2022-01-08 14:25:22 +0530

    Add files via upload

---
+++ UniversityCourse/src/main/java/com/univ/exception/ApplicantNotFoundException.java
@@ -0,0 +1,14 @@
+package com.univapp.exception;
+
+public class ApplicantNotFoundException extends RuntimeException{
+	
+	private String message;
+	
+	public ApplicantNotFoundException(String message) {
+		super(message);
+		this.message = message;
+	}
+	public ApplicantNotFoundException() {
+		
+	}
+}
